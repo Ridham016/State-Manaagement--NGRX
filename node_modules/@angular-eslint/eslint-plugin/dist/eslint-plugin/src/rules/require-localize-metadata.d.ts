666e08b37fbfa70edb2dbd36b3b16a9cac1aa5ea
Ridham016 <ridham.kanauji@gmail.com>
2023-02-15 10:29:01 +0530

    node_module removed

--- node_modules/@angular-eslint/eslint-plugin/dist/eslint-plugin/src/rules/require-localize-metadata.d.ts
+++
@@ -1,9 +0,0 @@
-declare type Options = [
-    {
-        readonly requireDescription?: boolean;
-    }
-];
-export declare type MessageIds = 'requireLocalizeMetadata';
-export declare const RULE_NAME = "require-localize-metadata";
-declare const _default: import("@typescript-eslint/utils/dist/ts-eslint/Rule").RuleModule<"requireLocalizeMetadata", Options, import("@typescript-eslint/utils/dist/ts-eslint/Rule").RuleListener>;
-export default _default;
